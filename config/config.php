18c3a24c8359ab00cda1176d83fe690f1f2482d4
Elis-prog <elisamanuel2025@hotmail.com>
2026-02-02 08:44:56 +0000

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,9 @@
+
+<?php
+    $dbuser="root";
+    $dbpass="";
+    $host="localhost";
+    $db="bd_sigestoque";
+    $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+    $con = mysqli_connect("$host","$dbuser","$dbpass","$db");
+?>
